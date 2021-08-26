2348dae3ecb3d8191833b1f52865c6d5b3ba5baf
nmp828 <nmp828@alumni.ku.dk>
2021-08-26 15:00:52 +0200

    new_path

--- pycor/config.py
+++ pycor/config.py
@@ -6,8 +6,8 @@ basename = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    DanNet_PATH = 'H:/CST_COR/data_modeller/DanNet'
-    word2vec_PATH = 'H:/CST_COR/data_modeller/word2vec/dsl_skipgram_2020_m5_f500_epoch2_w5.model.txtvectors'
+    DanNet_PATH = 'N:/HUM-NORS-cst-projekter/COR/data_modeller/DanNet'
+    word2vec_PATH = 'N:/HUM-NORS-cst-projekter/COR/data_modeller/word2vec/dsl_skipgram_2020_m5_f500_epoch2_w5.model.txtvectors'
 
 
 class ProductionConfig(Config):
