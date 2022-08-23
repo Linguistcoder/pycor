2cb74c456bd8a9713b108f4277555f6ac3204b19
nmp828 <BrownieGIT123>
2022-08-23 17:41:23 +0200

    outcomment spacy

--- pycor/utils/preprocess.py
+++ pycor/utils/preprocess.py
@@ -4,7 +4,7 @@ import nltk
 
 from pycor.utils.lemmatizer import get_lemmatised_sentence
 
-nlp = spacy.load("da_core_news_sm")
+#nlp = spacy.load("da_core_news_sm")
 # nltk.download('stopwords')
 
 stopwords = set(nltk.corpus.stopwords.words('danish'))
@@ -103,58 +103,58 @@ def clean_wcl(wcl):
         return 'vb.'
     elif 'adj' in wcl:
         return 'adj.'
-
-def form_in_sentence(sentence, form):
-    """Checks that a word form is in a sentence"""
-
-    sentence = sentence.strip()
-    lan = nlp(sentence)
-    lemma_sentence = [token.lemma_ for token in lan]
-    text_sentence = [token.text for token in lan]
-    #form = nlp(form)[0].lemma_
-
-    if form in lemma_sentence:
-        form_index = [i for i, w in enumerate(lemma_sentence) if w == form]
-        text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(text_sentence)]
-        return ' '.join(text_sentence)
-
-    elif form in text_sentence:
-        form_index = [i for i, w in enumerate(text_sentence) if w == form]
-        text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(text_sentence)]
-        return ' '.join(text_sentence)
-
-    else:
-        lemma_sentence = get_lemmatised_sentence(sentence).split()
-        if form in lemma_sentence:
-            form_index = [i for i, w in enumerate(lemma_sentence) if w==form]
-            text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
-            return ' '.join(text_sentence)
-
-        else:
-            print(f'Form "{form}" cannot be found in the sentence:\n{sentence}')
-            print([(i, s) for i, s in enumerate(sentence.split())])
-
-            form_index = input('Please input correct index: ')
-            form_index = int(form_index) if form_index != '' else 0
-            text_sentence = [s if i != form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
-            print(' '.join(text_sentence) + '\n')
-            return ' '.join(text_sentence)
-
-
-
-def form_in_sentence2(sentence, form):
-    """Checks that a word form is in a sentence"""
-
-    sentence = sentence.strip()
-    lemma_sentence = get_lemmatised_sentence(sentence).split()
-
-    if form in lemma_sentence:
-        form_index = lemma_sentence.index(form)
-        text_sentence = [s if i != form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
-        return ' '.join(text_sentence)
-
-    else:
-        print(f'Form "{form}" cannot be found in the sentence:\n{sentence}')
-        #sentence_input = input('Please input correct sentence:')
-        sentence_input = 'no target'
-        return sentence_input
+#
+# def form_in_sentence(sentence, form):
+#     """Checks that a word form is in a sentence"""
+#
+#     sentence = sentence.strip()
+#     lan = nlp(sentence)
+#     lemma_sentence = [token.lemma_ for token in lan]
+#     text_sentence = [token.text for token in lan]
+#     #form = nlp(form)[0].lemma_
+#
+#     if form in lemma_sentence:
+#         form_index = [i for i, w in enumerate(lemma_sentence) if w == form]
+#         text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(text_sentence)]
+#         return ' '.join(text_sentence)
+#
+#     elif form in text_sentence:
+#         form_index = [i for i, w in enumerate(text_sentence) if w == form]
+#         text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(text_sentence)]
+#         return ' '.join(text_sentence)
+#
+#     else:
+#         lemma_sentence = get_lemmatised_sentence(sentence).split()
+#         if form in lemma_sentence:
+#             form_index = [i for i, w in enumerate(lemma_sentence) if w==form]
+#             text_sentence = [s if i not in form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
+#             return ' '.join(text_sentence)
+#
+#         else:
+#             print(f'Form "{form}" cannot be found in the sentence:\n{sentence}')
+#             print([(i, s) for i, s in enumerate(sentence.split())])
+#
+#             form_index = input('Please input correct index: ')
+#             form_index = int(form_index) if form_index != '' else 0
+#             text_sentence = [s if i != form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
+#             print(' '.join(text_sentence) + '\n')
+#             return ' '.join(text_sentence)
+#
+#
+#
+# def form_in_sentence2(sentence, form):
+#     """Checks that a word form is in a sentence"""
+#
+#     sentence = sentence.strip()
+#     lemma_sentence = get_lemmatised_sentence(sentence).split()
+#
+#     if form in lemma_sentence:
+#         form_index = lemma_sentence.index(form)
+#         text_sentence = [s if i != form_index else f'[TGT] {s} [TGT]' for i, s in enumerate(sentence.split())]
+#         return ' '.join(text_sentence)
+#
+#     else:
+#         print(f'Form "{form}" cannot be found in the sentence:\n{sentence}')
+#         #sentence_input = input('Please input correct sentence:')
+#         sentence_input = 'no target'
+#         return sentence_input
